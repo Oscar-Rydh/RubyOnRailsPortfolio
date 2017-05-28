3bf26b574cbc32d01b01c468b8ebede007b47793
Oscar Rydh <oscar.rydh.93@gmail.com>
2017-05-28 21:41:52 +0200

    Added the index action for the portfolio

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,7 +32,7 @@ puts "5 skills created"
       body: "Lorem ipsum dolor sit amet, libero vivamus consequat justo, tellus fusce aliquam suscipit. Nam pede neque donec, vestibulum id ut interdum nec nullam diam. Semper convallis suscipit, libero dignissim donec molestie nunc mauris, arcu nisl fringilla sed, convallis eu consectetuer, praesent ex. Egestas id. Aenean proident leo sit in odio, pretium turpis, curabitur scelerisque. Donec suspendisse.
              At nullam in aliquam euismod pellentesque sociis. Consequat nec. Ornare sit enim hymenaeos, ut suspendisse sociis feugiat elit magna. Volutpat id arcu per nunc orci, maecenas vel. Ipsum at pellentesque dui quisque amet, ornare elit convallis sapien lectus, eros pellentesque et iaculis tristique nunc. Varius luctus justo nec nullam vehicula. Nibh arcu est lacus mus, velit sem natoque porttitor sagittis, praesent accumsan quis, sit a. Et wisi donec fermentum et at, netus arcu risus, tellus libero eu quam pretium ipsum et, ridiculus id elementum velit etiam nulla.",
       main_image: "http://placehold.it/600x400",
-      thumb_image: "http://placehold.it/300x200"
+      thumb_image: "http://placehold.it/350x200"
   )
 end
 
